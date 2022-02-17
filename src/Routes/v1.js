cef15e20aa85a795ba0b7cec1bb63c008c30b2de
Maxim <73140396+MaximKing1@users.noreply.github.com>
2022-02-17 14:46:08 +0000

    V1 added routes

--- src/Routes/v1.js
+++ src/Routes/v1.js
@@ -1,7 +1,19 @@
 const config = require('../../config');
 
 module.exports = async function (fastify, opts, done) {
-  fastify.post('/', async (request, reply) => {
+  fastify.get('/public/get/:id', async (request, reply) => {
+
+  });
+
+  fastify.get('/public/info/:id', async (request, reply) => {
+
+  });
+
+  fastify.post('/public/upload/:id', async (request, reply) => {
+
+  });
+
+  fastify.post('/public/delete/:id', async (request, reply) => {
 
   });
 };
