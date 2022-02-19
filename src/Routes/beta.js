9c24f5288d418b881301d188db4d94bf28b47c5d
Maxim King <73140396+MaximKing1@users.noreply.github.com>
2022-02-19 09:21:28 +0000

    Beta Routes

--- src/Routes/beta.js
+++ src/Routes/beta.js
@@ -1,7 +1,122 @@
 const config = require('../../config');
+const bcrypt = require('bcrypt');
+const xssFilters = require('xss-filters');
 
 module.exports = async function (fastify, opts, done) {
-  fastify.post('/', async (request, reply) => {
+  fastify.get('/public/get/:id', async (request, reply) => {
+    const ID = request.params.id;
+  }); // Public
 
-  });
+  fastify.get('/public/info/:id', async (request, reply) => {
+    const ID = request.params.id;
+  }); // Public
+
+  fastify.post('/public/upload/:id', async (request, reply) => {
+    const ID = request.params.id;
+  }); // Public
+
+  fastify.post('/private/delete/:id', async (request, reply) => {
+    const ID = request.params.id;
+    const email = xssFilters.inHTMLData(request.body.email);
+    const password = request.body.password;
+    const UserManager = app.UserManager.findOne({
+      email: email,
+    }).then((user) => {
+      if (!user) {
+        reply.status(401).send({
+          error: true,
+          email: 'Incorrect Email Address or Password',
+        });
+      }
+      bcrypt.compare(password, user.password).then((isMatch) => {
+        if (isMatch) {
+          // Send Data HEre
+        }
+      });
+    });
+  }); // Private (Username & Pass Protected)
+
+  fastify.get('/private/get/:id', async (request, reply) => {
+    const ID = request.params.id;
+    const email = xssFilters.inHTMLData(request.body.email);
+    const password = request.body.password;
+    const UserManager = app.UserManager.findOne({
+      email: email,
+    }).then((user) => {
+      if (!user) {
+        reply.status(401).send({
+          error: true,
+          email: 'Incorrect Email Address or Password',
+        });
+      }
+      bcrypt.compare(password, user.password).then((isMatch) => {
+        if (isMatch) {
+          // Send Data HEre
+        }
+      });
+    });
+  }); // Private (Username & Pass Protected)
+
+  fastify.get('/private/info/:id', async (request, reply) => {
+    const ID = request.params.id;
+    const email = xssFilters.inHTMLData(request.body.email);
+    const password = request.body.password;
+    const UserManager = app.UserManager.findOne({
+      email: email,
+    }).then((user) => {
+      if (!user) {
+        reply.status(401).send({
+          error: true,
+          email: 'Incorrect Email Address or Password',
+        });
+      }
+      bcrypt.compare(password, user.password).then((isMatch) => {
+        if (isMatch) {
+          // Send Data HEre
+        }
+      });
+    });
+  }); // Private (Username & Pass Protected)
+
+  fastify.post('/private/upload/:id', async (request, reply) => {
+    const ID = request.params.id;
+    const email = xssFilters.inHTMLData(request.body.email);
+    const password = request.body.password;
+    const UserManager = app.UserManager.findOne({
+      email: email,
+    }).then((user) => {
+      if (!user) {
+        reply.status(401).send({
+          error: true,
+          email: 'Incorrect Email Address or Password',
+        });
+      }
+      bcrypt.compare(password, user.password).then((isMatch) => {
+        if (isMatch) {
+          // Send Data HEre
+        }
+      });
+    });
+  }); // Private (Username & Pass Protected)
+
+  fastify.post('/private/delete/:id', async (request, reply) => {
+    const ID = request.params.id;
+    const email = xssFilters.inHTMLData(request.body.email);
+    const password = request.body.password;
+    const UserManager = app.UserManager.findOne({
+      email: email,
+    }).then((user) => {
+      if (!user) {
+        reply.status(401).send({
+          error: true,
+          email: 'Incorrect Email Address or Password',
+        });
+      }
+      bcrypt.compare(password, user.password).then((isMatch) => {
+        if (isMatch) {
+          // Send Data HEre
+        }
+      });
+    });
+  }); // Private (Username & Pass Protected)
 };
