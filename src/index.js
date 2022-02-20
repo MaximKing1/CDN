dfcf23cde137f1d8f3c135b655d09af636ef7a88
Maxim <73140396+MaximKing1@users.noreply.github.com>
2022-02-20 23:07:13 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ const app = require('fastify')({ logger: false });
 require('./Modules/fastify')(app);
 require('./Modules/collections')(app);
 require('./Modules/routers')(app);
+
+setInterval(async function() {
+   await require('./Modules/pullFiles')(app)
+}, 300000);
