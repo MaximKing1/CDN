f32089cf7ff0316568ffae681c90ada16c91f3b8
Maxim <73140396+MaximKing1@users.noreply.github.com>
2022-02-21 18:20:36 +0000

    Update database.js

--- src/Modules/database.js
+++ src/Modules/database.js
@@ -1,5 +1,5 @@
 const WasabiS3 = require('wasabi-s3');
-const config = require('../../config');
+const config = require('../../config.slave.js');
 
 module.exports = async (app) => {
   global.wasabi = new WasabiS3('wasabi', {
