542c939cef6fd34971e0324abec8c0d9dba28f1d
Maxim <73140396+MaximKing1@users.noreply.github.com>
2022-02-21 20:13:17 +0000

    Update config.slave.js

--- src/config.slave.js
+++ src/config.slave.js
@@ -2,6 +2,8 @@ exports.adminPassword =
   'yg6g4byugeuybygbiygyugbyuftuftufvtufvtfytdfcytftufytfc';
 exports.PORT = '3005';
 
+exports.files = 's3'; // 's3' or 'local', Where the CDN files are downloaded from
+
 exports.MongoDB = 'mongodb://';
 exports.accessKeyID = '';
 exports.secretAccessKey = '';
