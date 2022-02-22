7da991601338d3e394ef5c0d64731c95f73f3bbf
Maxim <73140396+MaximKing1@users.noreply.github.com>
2022-02-22 21:01:45 +0000

    Update pullFiles.js

--- src/Functions/pullFiles.js
+++ src/Functions/pullFiles.js
@@ -1,3 +1,9 @@
-module.exports = () => {
+const config = require('../../config.slave.js');
 
+module.exports = () => {
+   if(config.files == "local") {
+      // Run Local Script
+   } else if(config.files == "s3") {
+      // Run Pull S3 Files
+   }
 }
