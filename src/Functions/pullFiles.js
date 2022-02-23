8b50f826b4a4edce81b817d7793e51a897d4d5ba
Maxim King <73140396+MaximKing1@users.noreply.github.com>
2022-02-23 08:06:42 +0000

    Pull Files Func
    Pull Files Function.

--- src/Functions/pullFiles.js
+++ src/Functions/pullFiles.js
@@ -1,9 +1,25 @@
 const config = require('./../config.slave.js');
+const chalk = require('chalk');
 
-module.exports = () => {
-   if(config.files == "local") {
-      // Run Local Script
-   } else if(config.files == "s3") {
-      // Run Pull S3 Files
-   }
-}
+module.exports = (app) => {
+  if (config.files == 'local') {
+    // Run Local Script
+  } else if (config.files == 's3') {
+    let bucketName = 'example';
+    let remoteFilename = 'file.bin';
+    let downloadedFilePath = '/tmp';
+    global.wasabi
+      .downloadFile(bucketName, remoteFilename, downloadedFilePath)
+      .then((resp) => {
+        if (resp.status === 200) {
+          console.log(resp.message);
+          // Output: Bucket "example" was deleted successfully
+        }
+      })
+      .catch((resp) => {
+        if (resp.status === 400) {
+           console.log(chalk.red('[ WASABI S3 ]'), resp.message);
+        }
+      });
+  }
+};
