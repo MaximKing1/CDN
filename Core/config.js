9f78bdb0579bc3e1d7af6a51d75ef64d4640c4b7
MaximKing1 <73140396+MaximKing1@users.noreply.github.com>
2022-05-23 21:07:21 +0100

    Config File For Locations Endpoints

---
+++ Core/config.js
@@ -0,0 +1,7 @@
+exports.locations = {
+  GB: 'https://cdn.gb.example.com',
+  US: 'https://cdn.us.example.com',
+  SG: 'https://cdn.sg.example.com',
+  GE: 'https://cdn.ge.example.com',
+  TW: 'https://cdn.tw.example.com',
+};
