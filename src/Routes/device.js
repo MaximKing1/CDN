352c5fcf1a54bb949d76038ad0e7fd53776dae1e
MaximKing1 <73140396+MaximKing1@users.noreply.github.com>
2022-05-26 21:16:29 +0100

    Importing Device Funcs

--- src/Routes/device.js
+++ src/Routes/device.js
@@ -1,1 +1,8 @@
+const {
+  restartDevice,
+  shutdownDevice,
+  logOffDevice,
+  sleeptDevice,
+} = require('../deviceFuncs/deviceFuncs');
+
 module.exports = async function (fastify, opts, done) {};
