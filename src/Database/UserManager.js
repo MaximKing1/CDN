78fa3765ea8b374e49c32b1ab2c49065ec6a5c4c
MaximKing1 <73140396+MaximKing1@users.noreply.github.com>
2022-05-26 20:31:20 +0100

    Updated Models

--- src/Database/UserManager.js
+++ src/Database/UserManager.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose');
 
-const UserManager = mongoose.Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now() },
-  ip: { type: String, required: true },
-});
-
-// export model user with UserSchema
-module.exports = mongoose.model('UserManager', UserManager);
+module.exports = model(
+  'UserManager',
+  Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now() },
+    ip: { type: String, required: true },
+  })
+);
