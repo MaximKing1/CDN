83ecea54268279089e4633b858f4e11898d787a5
MaximKing1 <73140396+MaximKing1@users.noreply.github.com>
2022-05-26 21:28:30 +0100

    UserID Checks

--- src/Database/UserManager.js
+++ src/Database/UserManager.js
@@ -3,6 +3,7 @@ const { Schema, model } = require('mongoose');
 module.exports = model(
   'UserManager',
   Schema({
+    userID: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
     createdAt: { type: Date, default: Date.now() },
--- src/Routes/auth.js
+++ src/Routes/auth.js
@@ -1,16 +1,32 @@
 const { genSalt, compare, hash } = require('bcrypt');
 const { inHTMLData } = require('xss-filters');
 
+const random = (length = 8) => {
+  let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let str = '';
+  for (let i = 0; i < length; i++) {
+    str += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return str;
+};
+
 module.exports = async function (fastify, opts, done) {
   fastify.post('/addUser', async (request, reply) => {
     if (request.headers.auth === process.env.adminPassword) {
       return reply.send('Incorrect Admin Password');
     } // Checks Admin Password In Config For Adding Users
 
+    var userID = random(20);
     const email = inHTMLData(request.body.email);
-    const UserManager = app.UserManager.findOne({ email: email });
+
+    if (await app.UserManager.findOne({ userID: userID })) {
+      userID = random(21);
+    }
+
+    const UserManager = await app.UserManager.findOne({ email: email });
     if (!UserManager) {
       new app.UserManager({
+        userID: userID,
         email: email,
         password: null,
       }).then(async (newUserManager) => {
