4405b46d301eca273aab7b6e5d5eb72f34d4d766
MaximKing1 <73140396+MaximKing1@users.noreply.github.com>
2023-10-11 23:19:18 +0100

    Fixed V1 Routes

--- src/Routes/v1.js
+++ src/Routes/v1.js
@@ -2,103 +2,77 @@ const bcrypt = require('bcrypt');
 const xssFilters = require('xss-filters');
 const readFile = require('../Functions/readFile');
 
+async function authenticate(email, password) {
+  const user = await app.UserManager.findOne({
+    email: xssFilters.inHTMLData(email),
+  });
+
+  if (!user) {
+    return { error: true, message: 'Incorrect Email Address or Password' };
+  }
+
+  const isAMatch = await bcrypt.compare(password, user.password);
+
+  if (!isAMatch) {
+    return { error: true, message: 'Incorrect Email Address or Password' };
+  }
+
+  return { user: user, auth: true, message: 'Correct User Details' };
+}
+
 module.exports = async function (fastify, opts, done) {
   fastify.get('/public/get/:id', async (request, reply) => {
     const ID = request.params.id;
     const file = readFile(ID);
-  }); // Public
+    // Handle the file data, possibly sending it in the response.
+    reply.send({ file });
+  });
 
   fastify.get('/public/info/:id', async (request, reply) => {
     const ID = request.params.id;
     const file = readFile(ID);
-  }); // Public
+  
+    reply.send({ info: file });
+  });
 
   fastify.post('/public/upload/:id', async (request, reply) => {
     const ID = request.params.id;
     const file = request.body.file;
-  }); // Public
+    // Handle the file upload logic here.
+    reply.send({ success: true });
+  });
 
   fastify.post('/private/delete/:id', async (request, reply) => {
+    const { email, password } = request.body;
+    const auth = await authenticate(email, password);
+    if (auth.error) {
+      return reply.status(401).send(auth);
+    }
     const ID = request.params.id;
-    const email = xssFilters.inHTMLData(request.body.email);
-    const password = request.body.password;
-    const UserManager = app.UserManager.findOne({
-      email: email,
-    }).then((user) => {
-      if (!user) {
-        reply.status(401).send({
-          error: true,
-          email: 'Incorrect Email Address or Password',
-        });
-      }
-      bcrypt.compare(password, user.password).then((isMatch) => {
-        if (isMatch) {
-          // Send Data HEre
-        }
-      });
-    });
-  }); // Private (Username & Pass Protected)
+    // Handle the delete logic here.
+    reply.send({ success: true });
+  });
 
-  fastify.get('/private/get/:id', async (request, reply) => {
-    const ID = request.params.id;
-    const email = xssFilters.inHTMLData(request.body.email);
-    const password = request.body.password;
-    const UserManager = await app.UserManager.findOne({
-      email: email,
-    }).then((user) => {
-      if (!user) {
-        reply.status(401).send({
-          error: true,
-          email: 'Incorrect Email Address or Password',
-        });
-      }
-      bcrypt.compare(password, user.password).then((isMatch) => {
-        if (isMatch) {
-          // Send Data Here
-        }
-      });
-    });
-  }); // Private (Username & Pass Protected)
+  ['/private/get/:id', '/private/info/:id', '/private/upload/:id'].forEach(
+    (endpoint) => {
+      fastify.route({
+        method: ['GET', 'POST'],
+        url: endpoint,
+        handler: async (request, reply) => {
+          const { email, password } = request.body;
+          const auth = await authenticate(email, password);
+          if (auth.error) {
+            return reply.status(401).send(auth);
+          }
+          const ID = request.params.id;
+          
+          // TODO: Handle the logic here.
 
-  fastify.get('/private/info/:id', async (request, reply) => {
-    const ID = request.params.id;
-    const email = xssFilters.inHTMLData(request.body.email);
-    const password = request.body.password;
-    const UserManager = await app.UserManager.findOne({
-      email: email,
-    }).then((user) => {
-      if (!user) {
-        reply.status(401).send({
-          error: true,
-          email: 'Incorrect Email Address or Password',
-        });
-      }
-      bcrypt.compare(password, user.password).then((isMatch) => {
-        if (isMatch) {
-          // Send Data Here
-        }
+          reply.send({ success: true });
+        },
       });
-    });
-  }); // Private (Username & Pass Protected)
+    }
+  );
 
-  fastify.post('/private/upload/:id', async (request, reply) => {
-    const ID = request.params.id;
-    const email = xssFilters.inHTMLData(request.body.email);
-    const password = request.body.password;
-    const UserManager = await app.UserManager.findOne({
-      email: email,
-    }).then((user) => {
-      if (!user) {
-        reply.status(401).send({
-          error: true,
-          email: 'Incorrect Email Address or Password',
-        });
-      }
-      bcrypt.compare(password, user.password).then((isMatch) => {
-        if (isMatch) {
-          // Send Data Here
-        }
-      });
-    });
-  }); // Private (Username & Pass Protected)
+  done();
 };
