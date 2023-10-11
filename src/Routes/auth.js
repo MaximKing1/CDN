9acbb19768acaf628f2a093b85f1407dd41fa8d2
MaximKing1 <73140396+MaximKing1@users.noreply.github.com>
2023-10-11 23:08:12 +0100

    Fixed the Device Functions and Auth Functions improvements

--- src/Routes/auth.js
+++ src/Routes/auth.js
@@ -2,64 +2,88 @@ const { genSalt, compare, hash } = require('bcrypt');
 const { inHTMLData } = require('xss-filters');
 
 const random = (length = 8) => {
-  let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let str = '';
-  for (let i = 0; i < length; i++) {
-    str += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-  return str;
+  const chars =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  return Array.from({ length })
+    .map(() => chars.charAt(Math.floor(Math.random() * chars.length)))
+    .join('');
 };
 
 module.exports = async function (fastify, opts, done) {
   fastify.post('/addUser', async (request, reply) => {
-    if (request.headers.auth === process.env.adminPassword) {
-      return reply.send('Incorrect Admin Password');
-    } // Checks Admin Password In Config For Adding Users
+    try {
+      if (request.headers.auth !== process.env.adminPassword) {
+        return reply
+          .status(403)
+          .send({ error: true, message: 'Incorrect Admin Password' });
+      }
 
-    var userID = random(20);
-    const email = inHTMLData(request.body.email);
+      const email = inHTMLData(request.body.email);
+      const password = request.body.password; // Assuming you pass the password when adding a user.
 
-    if (await app.UserManager.findOne({ userID: userID })) {
-      userID = random(21);
-    }
+      let userID = random(20);
+      while (await app.UserManager.findOne({ userID })) {
+        userID = random(21); // Increase length if collision.
+      }
 
-    const UserManager = await app.UserManager.findOne({ email: email });
-    if (!UserManager) {
-      new app.UserManager({
-        userID: userID,
-        email: email,
-        password: null,
-      }).then(async (newUserManager) => {
-        genSalt(10, (err, salt) => {
-          hash(newUserManager.password, salt, async (err, hash) => {
-            if (err) throw err;
-            newUserManager.password = hash;
-            newUserManager.ip = request.ip;
-            await newUserManager.save();
+      const existingUser = await app.UserManager.findOne({ email });
+      if (existingUser) {
+        return reply
+          .status(409)
+          .send({ error: true, message: 'Email already exists' });
+      }
+
+      genSalt(10, (err, salt) => {
+        if (err) throw err;
+        hash(password, salt, async (err, hashedPassword) => {
+          if (err) throw err;
+          const newUser = new app.UserManager({
+            userID,
+            email,
+            password: hashedPassword,
+            ip: request.ip,
           });
+
+          await newUser.save();
+          reply.send({ success: true, message: 'User added successfully' });
         });
       });
+    } catch (error) {
+      reply.status(500).send({ error: true, message: error.message });
     }
   });
 
   fastify.post('/checkAccount', async (request, reply) => {
-    const email = inHTMLData(request.body.email);
-    const password = request.body.password;
-    await app.UserManager.findOne({
-      email: email,
-    }).then(async (user) => {
+    try {
+      const email = inHTMLData(request.body.email);
+      const password = request.body.password;
+
+      const user = await app.UserManager.findOne({ email });
+
       if (!user) {
         return reply
           .status(401)
-          .send({ error: true, email: 'Incorrect Email Address or Password' });
+          .send({
+            error: true,
+            message: 'Incorrect Email Address or Password',
+          });
       }
-    });
-    await compare(password, user.password).then((isMatch) => {
+
+      const isMatch = await compare(password, user.password);
       if (isMatch) {
+        return reply.send({ success: true, message: 'Correct User Details' });
+      } else {
         return reply
-          .status(200)
-          .send({ error: false, message: 'Correct User Details' });
+          .status(401)
+          .send({
+            error: true,
+            message: 'Incorrect Email Address or Password',
+          });
       }
-    });
+    } catch (error) {
+      reply.status(500).send({ error: true, message: error.message });
+    }
   });
+
+  done();
 };
--- src/Routes/device.js
+++ src/Routes/device.js
@@ -2,9 +2,91 @@ const {
   restartDevice,
   shutdownDevice,
   logOffDevice,
-  sleeptDevice,
+  sleepDevice,
 } = require('../deviceFuncs/deviceFuncs');
+const { genSalt, compare, hash } = require('bcrypt');
+const { inHTMLData } = require('xss-filters');
 
 module.exports = async function (fastify, opts, done) {
-    
+  fastify.addHook('preHandler', async (request, reply, done) => {
+    const email = inHTMLData(request.body.email);
+    const password = request.body.password;
+
+    const user = await app.UserManager.findOne({ email });
+    if (!user) {
+      return reply
+        .status(401)
+        .send({ error: true, message: 'Incorrect Email Address or Password' });
+    }
+
+    const isMatch = await compare(password, user.password);
+    if (!isMatch) {
+      return reply
+        .status(401)
+        .send({ error: true, message: 'Incorrect Email Address or Password' });
+    }
+
+    done();
+  });
+
+  // Restart Device
+  fastify.get('/device/restart', async (request, reply) => {
+    try {
+      await restartDevice();
+      reply.send({ success: true, message: 'Device restarted successfully' });
+    } catch (error) {
+      reply.status(500).send({
+        success: false,
+        message: 'Failed to restart device',
+        error: error.message,
+      });
+    }
+  });
+
+  // Shutdown Device
+  fastify.get('/device/shutdown', async (request, reply) => {
+    try {
+      await shutdownDevice();
+      reply.send({ success: true, message: 'Device shutdown successfully' });
+    } catch (error) {
+      reply.status(500).send({
+        success: false,
+        message: 'Failed to shutdown device',
+        error: error.message,
+      });
+    }
+  });
+
+  // Log Off Device
+  fastify.get('/device/logoff', async (request, reply) => {
+    try {
+      await logOffDevice();
+      reply.send({ success: true, message: 'Device logged off successfully' });
+    } catch (error) {
+      reply.status(500).send({
+        success: false,
+        message: 'Failed to log off device',
+        error: error.message,
+      });
+    }
+  });
+
+  // Sleep Device
+  fastify.get('/device/sleep', async (request, reply) => {
+    try {
+      await sleepDevice();
+      reply.send({
+        success: true,
+        message: 'Device put to sleep successfully',
+      });
+    } catch (error) {
+      reply.status(500).send({
+        success: false,
+        message: 'Failed to put device to sleep',
+        error: error.message,
+      });
+    }
+  });
+
+  done();
 };
